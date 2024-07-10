e884b1df28600f662fdb0f5e1eb42a7a1c6cb871
memooh <m.ovdb@outlook.com>
2024-07-10 12:49:36 +0200

    Made the quickinfo section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import QuickInfo from './components/QuickInfo/QuickInfo'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <Header />
     <Hero />
+    <QuickInfo />
     <div>Hoi van app.jsx</div>
     </>
   )
---
+++ src/components/QuickInfo/QuickInfo.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./QuickInfo.css";
+
+const QuickInfo = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 text-center">
+            <h3 className="infoTitle">Project Plan</h3>
+            <p className="infoText">
+              There are many variations of the passages of lorem Ipsum from
+              available, majority.
+            </p>
+            <button className="infoButton">
+              <a href="#">Read More</a>
+            </button>
+          </div>
+          <div className="col-lg-4 text-center">
+            <h3 className="infoTitle">Interior Work</h3>
+            <p className="infoText">
+              There are many variations of the passages of lorem Ipsum from
+              available, majority.
+            </p>
+            <button className="infoButton">
+              <a href="#">Read More</a>
+            </button>
+          </div>
+          <div className="col-lg-4 text-center">
+            <h3 className="infoTitle">Realization</h3>
+            <p className="infoText">
+              There are many variations of the passages of lorem Ipsum from
+              available, majority.
+            </p>
+            <button className="infoButton">
+              <a href="#">Read More</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuickInfo;
